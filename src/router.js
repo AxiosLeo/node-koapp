c59d7e1928b21af981764ff6b8f32ccc390577e0
axiosleo <axiosleo@foxmail.com>
2022-07-30 10:15:25 +0800

    feat: Router

---
+++ src/router.js
@@ -0,0 +1,37 @@
+'use strict';
+
+class Router {
+  constructor(prefix, options = {}) {
+    this.prefix = prefix || '';
+    Object.assign(this, {
+      method: '',
+      handlers: [],
+      routers: [],
+      middlewares: [],
+    }, options || {});
+  }
+
+  /**
+   * @param {Router} router 
+   * @returns 
+   */
+  add(router) {
+    this.routers.push(router);
+    return this;
+  }
+
+  /**
+   * @param {string} prefix 
+   * @param {*} options 
+   * @returns 
+   */
+  new(prefix, options = {}) {
+    const router = new Router(prefix, options);
+    this.add(router);
+    return this;
+  }
+}
+
+module.exports = {
+  Router,
+};
