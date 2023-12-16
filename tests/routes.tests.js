71043d6d7595b1ab5cd44d56fdccfde4e8e2fc16
axiosleo <axiosleo@foxmail.com>
2023-12-17 01:30:14 +0800

    test: Add router test case

---
+++ tests/routes.tests.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { expect } = require('chai');
+const { Router } = require('../index');
+const { resolveRouters, getRouteInfo } = require('../src/core');
+
+describe('router test case', () => {
+  it('default router', () => {
+    const defaultOptions = {
+      method: 'any', handlers: [async () => { }]
+    };
+    const router = new Router('/api/v1', defaultOptions);
+    router.new('/***', defaultOptions);
+
+    const moduleRouters = new Router('/module');
+    router.add(moduleRouters);
+
+    const routes = resolveRouters([router]);
+    const result = getRouteInfo(routes, '/api/v1/module/123');
+    expect(result).not.to.be.null;
+  });
+});
