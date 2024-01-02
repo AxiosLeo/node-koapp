d3f2f6c2a2d104046549b9d02e838db376949ccd
axiosleo <axiosleo@foxmail.com>
2024-01-02 16:31:27 +0800

    feat: init command

--- commands/init.js
+++ commands/init.js
@@ -1,33 +1,56 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-const { Command, printer } = require('@axiosleo/cli-tool');
+const path = require('path');
+const { _search, _write, _mkdir } = require('@axiosleo/cli-tool/src/helper/fs');
+const { _foreach, _exec } = require('@axiosleo/cli-tool/src/helper/cmd');
+const { Command, printer, debug } = require('@axiosleo/cli-tool');
+const { _render_with_file } = require('@axiosleo/cli-tool/src/helper/str');
 
-/**
- * import more features
- * @import const { Workflow } = require('@axiosleo/cli-tool');
- * @import const { Configuration } = require('@axiosleo/cli-tool');
- * @import const { helper: { fs, cmd, is, obj, str } } = require('@axiosleo/cli-tool');
- * @import const { debug } = require('@axiosleo/cli-tool');
- * @import const { locales } = require('@axiosleo/cli-tool');
- */
 class InitCommand extends Command {
   constructor() {
     super({
       name: 'init',
       desc: ''
     });
-    // this.addArgument('name', 'desc', 'required', null);
-    // this.addOption('name', 'n', 'desc', 'required', null);
+    this.addArgument('name', 'Application name', 'required');
+    this.addOption('dir', 'd', 'output dir', 'optional', process.cwd());
   }
 
   /**
    * @param {*} args 
    * @param {*} options 
-   * @param {string[]} argList 
-   * @param {import('@axiosleo/cli-tool').App} app 
    */
-  async exec(args, options, argList, app) {
-    printer.println('this is init command');
+  async exec(args, options) {
+    let name = args.name;
+
+    printer.info(`Will initialize ${name} application`);
+
+    let dir = path.join(options.dir, name + '/');
+    printer.info(`Output dir: ${dir}`);
+    printer.info('Initializing...');
+    await _mkdir(dir);
+
+    const tmplDir = path.join(__dirname, '../assets/tmpl/');
+    const files = await _search(tmplDir, 'tmpl');
+
+    await _foreach(files, async (file) => {
+      let f = file.substring(0, file.length - 5);
+      f = f.replace(tmplDir, '');
+      f = path.join(dir, f);
+      let c = await _render_with_file(file, { name });
+      await _write(f, c);
+    });
+    printer.success('Initialized successfully');
+    debug.log({ dir });
+    if (!await this.confirm('install dependencies?', true)) {
+      return;
+    }
+    await _exec('npm install', dir);
+    if (!await this.confirm('start services right now?', true)) {
+      return;
+    }
+    await _exec('npm run dev:services', dir);
   }
 }
 
