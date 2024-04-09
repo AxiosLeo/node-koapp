cc733708f7daa44d88cfbdd9f32e0a7ae975a1a7
axiosleo <axiosleo@foxmail.com>
2024-04-09 15:08:53 +0800

    Bump 1.0.7

--- bin/koapp.js
+++ bin/koapp.js
@@ -9,7 +9,7 @@ const app = new App({
   name: 'koapp CLI',
   desc: 'koapp application generator',
   bin: 'koapp',
-  version: '1.0.6',
+  version: '1.0.7',
   commands_dir: path.join(__dirname, '../commands'),
 });
 
