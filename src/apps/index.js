ffdb9fdb4c73c51ab67d3f924e67e473ef15b807
axiosleo <axiosleo@foxmail.com>
2024-07-30 15:52:48 +0800

    feat: export NetSocketApplication

--- src/apps/index.js
+++ src/apps/index.js
@@ -2,10 +2,10 @@
 
 const Application = require('./app');
 const KoaApplication = require('./koa');
-const SocketApplication = require('./koa');
+const NetSocketApplication = require('./socket');
 
 module.exports = {
   Application,
   KoaApplication,
-  SocketApplication
+  NetSocketApplication
 };
