a2a16f93eee533b975eaaa1a6d91703c99985fc8
axiosleo <axiosleo@foxmail.com>
2025-07-07 16:37:46 +0800

    feat: implement API router and WebSocket server with HTML client for enhanced WebSocket communication

---
+++ examples/api.router.js
@@ -0,0 +1,30 @@
+const { success } = require('../src/response');
+
+const { Router } = require('..');
+const { debug } = require('@axiosleo/cli-tool');
+
+const root = new Router(null, {
+  middlewares: [async (context) => {
+    debug.log(`[${context.app_id}] ${context.method}: ${context.router.pathinfo}`);
+  }],
+  afters: [async (context) => {
+    // debug.log(context);
+  }]
+});
+
+root.get('/api/test/{:id}', async (context) => {
+  success({
+    query: context.query,
+    body: context.body,
+    test: context.params.id
+  });
+});
+
+root.any('/***', async (context) => {
+  success({
+    query: context.query,
+    body: context.body
+  });
+});
+
+module.exports = root;
--- examples/socket.server.js
+++ examples/socket.server.js
@@ -1,36 +1,8 @@
 'use strict';
 
-const { Router } = require('..');
 const SocketApplication = require('../src/apps/socket');
-const { success } = require('../src/response');
 
-// const { debug } = require('@axiosleo/cli-tool');
-
-const root = new Router(null, {
-  middlewares: [async (context) => {
-    // debug.log(`[${context.app_id}] ${context.method}: ${context.router.pathinfo}`);
-  }],
-  afters: [async (context) => {
-    // debug.log(context);
-  }]
-});
-
-root.get('/api/test/{:id}', async (context) => {
-  // debug.log(context.connection.write(JSON.stringify({ test: 123 })));
-  success({
-    query: context.query,
-    body: context.body,
-    test: 'Hello World!'
-  });
-});
-
-root.any('/***', async (context) => {
-  // debug.log(context.connection.write(JSON.stringify({ test: 123 })));
-  success({
-    query: context.query,
-    body: context.body
-  });
-});
+const root = require('./api.router');
 
 const app = new SocketApplication({
   routers: [root],
