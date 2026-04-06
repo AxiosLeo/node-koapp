39bcd68d4b2e1563f6ddf4dae1bdfce8bce6ffb7
axiosleo <axiosleo@foxmail.com>
2026-04-06 15:04:46 +0800

    chore: update ECMAScript version in ESLint configuration from 2018 to 2020

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ export default [...compat.extends("eslint:recommended"), {
             beforeEach: true,
         },
 
-        ecmaVersion: 2018,
+        ecmaVersion: 2020,
         sourceType: "commonjs",
 
         parserOptions: {
