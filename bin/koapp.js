102955d0f3173806f02cb5b7cbf92e7bf0b91dae
axiosleo <axiosleo@foxmail.com>
2026-09-16 14:20:34 +0800

    chore: bump version of @axiosleo/koapp to 1.3.3
    - Updated package.json and related documentation to reflect the new version.
    - Adjusted references in markdown files to indicate the latest version for context types.

--- bin/koapp.js
+++ bin/koapp.js
@@ -9,7 +9,7 @@ const app = new App({
   name: 'koapp CLI',
   desc: 'koapp application generator',
   bin: 'koapp',
-  version: '1.3.2',
+  version: '1.3.3',
   commands_dir: path.join(__dirname, '../commands'),
 });
 
